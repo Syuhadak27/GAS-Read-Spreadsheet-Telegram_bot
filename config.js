34a20ac6b9c6d3a31be9603c71d6396b857d8fd8
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2024-09-08 17:51:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-const spreadsheetId      = '1yQNzPZo0Q5RxracA6723xnNRd0Pk48iXxqi1I-qA8Mg';
+const spreadsheetId      = '';
 const dataOrderSheetName = 'database';
 const dataList           = 'list';
 const dataInout          = 'inout';
 const dataStok           = 'stok' ;
 const logSheetName       = 'Log';
 
-const botToken       = '7080544223:AAHXbHNJgQdvZK5cpYSrBGD0SL33SREj77E'; 
+const botToken       = ''; 
 const telegramApiUrl = `https://api.telegram.org/bot${botToken}`;
-const appsScriptUrl  = 'https://script.google.com/macros/s/AKfycbx0c1oncpf9J8Hjr4kWCygKZScfHsdeegxok6rOkvn1PM0-AqPEqFntjEoSYmOxBSlIsw/exec'; 
+const appsScriptUrl  = ''; //url hasil deploy 
